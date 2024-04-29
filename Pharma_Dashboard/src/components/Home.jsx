0899908ea171789cc66b1e901fcd69836c74c7eb
Tarun <taruncv516@gmail.com>
2024-04-30 00:17:35 +0530

    error

--- Pharma_Dashboard/src/components/Home.jsx
+++ Pharma_Dashboard/src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Sidemenu from "./Sidemenu";
-import Rightmenu from "../Rightmenu";
+import Rightmenu from "./components/Rightmenu";
 
 
 export default function Home() {
